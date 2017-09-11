18fa32ded3664cdac8df1cc498abefd017b836e2
David Grove Jorgensen <dgj@kb.dk>
2017-09-11 10:49:12 +0200

    authenticate user

--- lib/Auth/Source/BrugerbaseAuth.php
+++ lib/Auth/Source/BrugerbaseAuth.php
@@ -1,5 +1,48 @@
 <?php
-class sspmod_exampleauth_Auth_Source_UserPass extends sspmod_core_Auth_UserPassBase {
+class sspmod_KB_Auth_Source_BrugerbaseAuth extends sspmod_core_Auth_UserPassBase {
+	
+	private $brugerbase;
+	
+	 public function __construct($info, $config) {
+        assert('is_array($info)');
+        assert('is_array($config)');
+		
+        // Call the parent constructor first, as required by the interface
+        parent::__construct($info, $config);
+		
+		$this->brugerbase = new sspmod_KB_BrugerbaseClient($config['brugerbaseBaseURL']);
+	 }
+ 
+	  public function login($username, $password) {
+                assert('is_string($username)');
+                assert('is_string($password)');
+				
+			$user = $this->brugerbase->login($username,$password);	
+	        if ($user == NULL) {
+				throw new SimpleSAML_Error_Error('WRONGUSERPASS');			
+			}
+			
+			if ($this->brugerbase->expire($username) < 0) {
+				throw new SimpleSAML_Error_Exception('USERPASSEXPIRED');
+			}
+			
+			$attributes = array();
+			$attributes['eduPersonPricipalName'] = array($user['userId'].'@kb.dk');
+			$attributes['norEduPersonLIN'] = array($user['userId']);
+			$attributes['gn'] = array($user['gn']);
+			$attributes['sn'] = array($user['sn']);
+			$attributes['cn'] =	array($user['gn'].' '.$user['sn']);
+			
+			foreach($user['userAttributesesByUserId'] as $userattr) {
+				if ($userattr['attributeId'] === 'email') {
+					if(!array_key_exists('mail',$attributes)) {
+						$attributes['mail'] = array();
+					}
+					$attributes['mail'][] = $userattr['val'];
+				}
+			}
+			return $attributes;
+        }
 
 }
 ?>
--- lib/BrugerbaseClient.php
+++ lib/BrugerbaseClient.php
@@ -1,7 +1,61 @@
 <?php
-	class sspmod_kb_BrugerbaseClient {
+	class sspmod_KB_BrugerbaseClient {
 		
-	
+		private $baseURL;
+		
+		private $loginURI = '/admin/rest/login';
+		private $expireURI = '/admin/rest/expire';
+
+		public function __construct($baseURL) {
+			assert(is_string($baseURL));
+			
+			$this->baseURL = $baseURL;
+		}
+		
+		public function login($username,$password) {
+			$url = \SimpleSAML\Utils\HTTP::addURLParameters($this->baseURL . $this->loginURI,
+							array('id' => $username, 'password' => $password));
+								
+			list($result,$respHeaders) =  \SimpleSAML\Utils\HTTP::fetch($url,array(),TRUE);
+			if(!isset($respHeaders)) {
+				// No response headers, this means the request failed in some way, so re-use old data
+                SimpleSAML_Logger::error('No response from brugerbase loginservice ');
+				throw new SimpleSAML_Error_Exception('BRUGERBASENOTRESPONDING');		
+            } elseif(preg_match('@^HTTP/1\.[01]\s200\s@', $respHeaders[0])) {
+                // 200 response - login OK
+                return json_decode($result,TRUE);
+			} elseif(preg_match('@^HTTP/1\.[01]\s401\s@', $respHeaders[0])) {
+               // 401 response - invalid password
+                return NULL;
+			} elseif(preg_match('@^HTTP/1\.[01]\s404\s@', $respHeaders[0])) {
+               // 404 response - invalid password
+                return NULL;	
+           } else {
+			    SimpleSAML_Logger::error('Brugerbase login unexpected response code '.$respHeaders[0]);
+				throw new SimpleSAML_Error_Exception('BRUGERBASENOTRESPONDING');		
+		   }
+        }
+
+		public function expire($username) {
+			$url = \SimpleSAML\Utils\HTTP::addURLParameters($this->baseURL . $this->expireURI,
+							array('id' => $username));
+			list($result,$respHeaders) =  \SimpleSAML\Utils\HTTP::fetch($url,array(),TRUE);
+			
+			if(!isset($respHeaders)) {
+				// No response headers, this means the request failed in some way, so re-use old data
+                SimpleSAML_Logger::error('No response from brugerbase getting expire for user '.$username);
+				throw new SimpleSAML_Error_Error('BRUGERBASENOTRESPONDING');		
+			} elseif(preg_match('@^HTTP/1\.[01]\s200\s@', $respHeaders[0])) {
+                // 200 response - login OK
+				if (is_numeric($result)) 
+					return intval($result);
+				SimpleSAML_Logger::error('Brugerbase expire not a number '.$result);
+				throw new SimpleSAML_Error_Exception('BRUGERBASENOTRESPONDING');		
+			} else {
+			    SimpleSAML_Logger::error('Brugerbase expire unexpected response code '.$respHeaders[0]);
+				throw new SimpleSAML_Error_Exception('BRUGERBASENOTRESPONDING');		
+		   }
+		}
 		
 	}
 
