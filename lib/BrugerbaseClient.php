0cc5c507459471642a0fbbee6275114365107021
David Grove Jorgensen <dgj@kb.dk>
2017-09-13 11:23:23 +0200

    get fetch to return for response code 401 and 404
    add function to reset password for user

--- lib/BrugerbaseClient.php
+++ lib/BrugerbaseClient.php
@@ -5,6 +5,7 @@
 		
 		private $loginURI = '/admin/rest/login';
 		private $expireURI = '/admin/rest/expire';
+		private $resetPwdURI = '/admin/rest/resetpwd';
 
 		public function __construct($baseURL) {
 			assert(is_string($baseURL));
@@ -13,24 +14,26 @@
 		}
 		
 		public function login($username,$password) {
+			assert(is_string($username));
+			assert(is_string($password));
 			$url = \SimpleSAML\Utils\HTTP::addURLParameters($this->baseURL . $this->loginURI,
 							array('id' => $username, 'password' => $password));
-								
-			list($result,$respHeaders) =  \SimpleSAML\Utils\HTTP::fetch($url,array(),TRUE);
+			list($result, $respHeaders) = \SimpleSAML\Utils\HTTP::fetch($url,
+				array('http'=>array('ignore_errors' => TRUE)), TRUE);
 			if(!isset($respHeaders)) {
 				// No response headers, this means the request failed in some way, so re-use old data
                 SimpleSAML_Logger::error('No response from brugerbase loginservice ');
 				throw new SimpleSAML_Error_Exception('BRUGERBASENOTRESPONDING');		
             } elseif(preg_match('@^HTTP/1\.[01]\s200\s@', $respHeaders[0])) {
-                // 200 response - login OK
-                return json_decode($result,TRUE);
+				// 200 response - login OK
+				return json_decode($result,TRUE);
 			} elseif(preg_match('@^HTTP/1\.[01]\s401\s@', $respHeaders[0])) {
-               // 401 response - invalid password
-                return NULL;
+				// 200 response - login OK
+				return NULL;
 			} elseif(preg_match('@^HTTP/1\.[01]\s404\s@', $respHeaders[0])) {
-               // 404 response - invalid password
-                return NULL;	
-           } else {
+				// 200 response - login OK
+				return NULL;
+			} else {
 			    SimpleSAML_Logger::error('Brugerbase login unexpected response code '.$respHeaders[0]);
 				throw new SimpleSAML_Error_Exception('BRUGERBASENOTRESPONDING');		
 		   }
@@ -56,7 +59,41 @@
 				throw new SimpleSAML_Error_Exception('BRUGERBASENOTRESPONDING');		
 		   }
 		}
-		
-	}
 
+		public function resetpwd($userid,$newpass) {
+			assert(is_string($userid));
+			assert(is_string($newpass));
+			$postdata = http_build_query(
+				array(
+					'id' => $userid,
+					'password' => $newpass
+				)
+			);
+			$context = array('http' =>
+				array(
+					'method'  => 'POST',
+					'header'  => 'Content-type: application/x-www-form-urlencoded',
+					'content' => $postdata
+				)
+			);
+			$url = $this->baseURL . $this->resetPwdURI;
+			list($result,$respHeaders) =  \SimpleSAML\Utils\HTTP::fetch($url,$context,TRUE);
+			if(!isset($respHeaders)) {
+				// No response headers, this means the request failed in some way, so re-use old data
+				SimpleSAML_Logger::error('No response from brugerbase reset password for '.$userid);
+				throw new SimpleSAML_Error_Exception('BRUGERBASENOTRESPONDING');
+			} elseif(preg_match('@^HTTP/1\.[01]\s200\s@', $respHeaders[0])) {
+				// 200 response - login OK
+				return TRUE;
+			} elseif(preg_match('@^HTTP/1\.[01]\s500\s@', $respHeaders[0])) {
+				SimpleSAML_Logger::error('reset password Internal server error '.$respHeaders[0]);
+				throw new SimpleSAML_Error_Exception('BRUGERBASENOTRESPONDING');
+			} else {
+				SimpleSAML_Logger::error('Brugerbase expire unexpected response code '.$respHeaders[0]);
+				throw new SimpleSAML_Error_Exception('BRUGERBASENOTRESPONDING');
+			}
+			// we should never get here
+			assert(false);
+		}
+	}
 ?>
