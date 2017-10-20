86ee580c62d3ccc980d9190ef5cf628e631a5110
David Grove Jorgensen <dgj@kb.dk>
2017-10-20 12:18:54 +0200

    added logout

---
+++ www/logout.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!array_key_exists('stateId', $_GET)) {
+    throw new SimpleSAML_Error_BadRequest('Missing required stateId query parameter.');
+}
+$state = SimpleSAML_Auth_State::loadState($_GET['stateId'], 'KB:fetchlocal');
+$state['saml:sp:State']['SimpleSAML_Auth_Source.Return'] = 'http://www.google.dk';
+SimpleSAML_Auth_ProcessingChain::resumeProcessing($state);
+
+assert('FALSE');
