73c9e4bc2a32c5ad734ce3e22291ea295d07bb38
David Grove Jorgensen <dgj@kb.dk>
2017-10-25 16:32:14 +0200

    better errors

--- templates/fetcherror.php
+++ templates/fetcherror.php
@@ -1,13 +1,14 @@
 <?php
 include(SimpleSAML_Module::getModuleDir('KB').'/templates/includes/header.php');
+$errcode = $this->data['errcode'];
 ?>
 <section id="content">
     <div class="container">
         <div class="col-md-6 col-md-offset-3">
             <div class="well well-lg info ">
                 <div id="msg" class="alert alert-danger">
-                    <h1>Fejl ved henting af brugerdata</h1>
-                    <p>Dit RUC id er tilknytte flere forskellige konti</p>
+                    <h1><?php echo $this->t('{KB:KB:fetcherror_header_' . $errcode . '}') ?></h1>
+                    <div><?php echo $this->t('{KB:KB:fetcherror_text_' . $errcode . '}') ?></div>
                 </div>
             </div>
         </div>
