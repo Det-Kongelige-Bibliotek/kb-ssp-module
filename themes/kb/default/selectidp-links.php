14f1a8981637bae658aa8f9097df65f7b91884f4
David Grove Jorgensen <dgj@kb.dk>
2017-10-27 12:21:24 +0200

    removed preferred idp

--- themes/kb/default/selectidp-links.php
+++ themes/kb/default/selectidp-links.php
@@ -43,31 +43,9 @@ foreach ($this->data['idplist'] as $idpentry) {
                                 }
                                 ?></p>
                             <?php
-                            if (!empty($this->data['preferredidp']) &&
-                                array_key_exists($this->data['preferredidp'], $this->data['idplist'])
-                            ) {
-                                $idpentry = $this->data['idplist'][$this->data['preferredidp']];
-                                echo '<div class="preferredidp">';
 
-                                if (array_key_exists('icon', $idpentry) && $idpentry['icon'] !== null) {
-                                    $iconUrl = \SimpleSAML\Utils\HTTP::resolveURL($idpentry['icon']);
-                                    echo '<img class="float-l" style="margin: 1em; padding: 3px; border: 1px solid #999" src="' .
-                                        htmlspecialchars($iconUrl) . '" />';
-                                }
-                                echo '<button type="submit" class="btn btn-success" name="idp_' . htmlspecialchars($idpentry['entityid']) . '">';
-                                echo '<h3 style="margin-top: 8px">' . htmlspecialchars($this->t('idpname_' . $idpentry['entityid'])) . '</h3>';
-                                if (!empty($idpentry['description'])) {
-                                    echo '	<p>' . htmlspecialchars($this->t('idpdesc_' . $idpentry['entityid'])) . '</p>';
-                                }
-                                echo '</button>';
-                                echo '	<img src="/' . $this->data['baseurlpath'] .
-                                    'resources/icons/experience/gtk-about.64x64.png" class="float-r" alt="' .
-                                    $this->t('icon_prefered_idp') . '" />';
-                                echo '</div>';
-                            }
 
                             foreach ($this->data['idplist'] as $idpentry) {
-                                if ($idpentry['entityid'] != $this->data['preferredidp']) {
                                     if (array_key_exists('icon', $idpentry) && $idpentry['icon'] !== null) {
                                         $iconUrl = \SimpleSAML\Utils\HTTP::resolveURL($idpentry['icon']);
                                         echo '<img class="float-l" style="clear: both; margin: 1em; padding: 3px; border: 1px solid #999"' .
@@ -80,7 +58,7 @@ foreach ($this->data['idplist'] as $idpentry) {
                                         echo '	<p>' . htmlspecialchars($this->t('idpdesc_' . $idpentry['entityid'])) . '</p>';
                                     }
                                     echo '</button>';
-                                }
+                                
                             }
                             ?>
                         </form>
