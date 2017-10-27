968bf139ae61c8e17b879b7e202ed48e266aee7b
David Grove Jorgensen <dgj@kb.dk>
2017-10-27 16:28:43 +0200

    even better error handling

--- lib/ErrorShow.php
+++ lib/ErrorShow.php
@@ -2,7 +2,7 @@
 class sspmod_KB_ErrorShow {
 
     public static function show($config=null, $data=null) {
-        if (in_array($data['errorCode'],array("CONFIGERROR"))) {
+        if (in_array($data['errorCode'],array("CONFIGERROR","REMOTEATTRNOVALUE","TOOMANYUSERS"))) {
             $t = new SimpleSAML_XHTML_Template($config, 'KB:error.php');
             $t->data = array_merge($t->data, $data);
             $t->show();
