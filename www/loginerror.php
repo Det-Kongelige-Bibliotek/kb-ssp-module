ab040cf6316f9bc7a0d8dd6c8374c8890b674b41
David Grove Jorgensen <dgj@kb.dk>
2018-01-12 10:24:48 +0100

    fix recovery url error

--- www/loginerror.php
+++ www/loginerror.php
@@ -1,7 +1,7 @@
 <?php
     $globalConfig = SimpleSAML_Configuration::getInstance();
     $t = new SimpleSAML_XHTML_Template($globalConfig, 'KB:loginerror.php');
-    $t->data['recoveryUrl'] = $globalConfig->getValue('brugerbase.baseurl').'/user/recovery';
+    $t->data['recoveryUrl'] = $globalConfig->getValue('brugerbase.baseurl').'/recovery';
     $t->data['errorcode'] = $_GET['errorcode'];
     $t->show();
 ?>
