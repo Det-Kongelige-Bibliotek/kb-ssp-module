f2e00551cace5c74fb72bc69cc44a273bbc59c67
David Grove Jorgensen <dgj@kb.dk>
2018-02-08 14:52:38 +0100

    fixed ispassive problem

--- lib/IdPDisco.php
+++ lib/IdPDisco.php
@@ -5,7 +5,20 @@
 class sspmod_KB_IdPDisco extends SimpleSAML_XHTML_IdPDisco
 {
 
-    /**
+    private $isPassive = false;
+
+    public function __construct(array $metadataSets, $instance)
+    {
+        parent::__construct($metadataSets,$instance);
+
+        if (array_key_exists('isPassive', $_GET)) {
+            if ($_GET['isPassive'] === 'true') {
+                $this->isPassive = true;
+            }
+        }
+    }
+
+        /**
      *
      */
     protected function getSavedIdP()
@@ -18,13 +31,14 @@ class sspmod_KB_IdPDisco extends SimpleSAML_XHTML_IdPDisco
         }
 
         if ($this->getCookie('remember') === '1' || $this-isPassive) {
-            $this->log('Return previously saved IdP because of remember cookie set to 1 or because of isPassive');
+            $this->log('Return previously saved IdP because of remember cookie set to 1 or because of isPassive ');
             $prevIdP =  $this->getPreviousIdP();
         }
 
 	    $IDPList = $this->getScopedIDPList();
         if (!empty($IDPList) && !array_key_exists($prevIdP,$IDPList)) {
             $this->log('Previously saved IdP is not ind scoped IDPList');
+            $this->setCookie('remember', '0');
             return null;
 	    }
 
